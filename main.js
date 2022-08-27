e156e15d1b58354f94f3ee8784034ee1bac74933
Linet Oliech <oliechlin@gmail.com>
2022-08-27 22:59:38 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal')
+modal.className = 'hidden'
+
+const hearts = document.querySelectorAll('.like-glyph')
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+
+    .then(() => {
+      if (heart.innerHTML === EMPTY_HEART) {
+        heart.innerHTML = FULL_HEART
+        heart.className = 'activated-heart'
+      } else {
+        heart.innerHTML = EMPTY_HEART
+        heart.className = ''
+      }
+    })
+.catch(error => {
+      modal.className = ''
+      modal.innerText = error
+      setTimeout(() => {
+        modal.className = 'hidden'
+      }, 2000)
+    })
+  })
+})
 
 
 
